45657c048a077e91c39729a8551effb016ecbc8c
ShinyBread <shinybreadjp@gmail.com>
2024-05-23 16:30:44 -0400

    silly-makefun-page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../app.css";
+    import ViewTransition from "$lib/components/ViewTransition.svelte"
+  </script>
+  
+  <svelte:head>
+    <title>Usuck</title>
+  </svelte:head>
+
+  <div class=app>
+    <ViewTransition />
+    <main class="min-h-screen ">
+        <slot />
+    </main>
+</div>
